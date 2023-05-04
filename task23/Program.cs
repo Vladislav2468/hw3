2ae59ec59c0601ae2a364d40b4f28554982a6cd7
Vladislav Ivanov <lolowkapogba2468@gmail.com>
2023-05-04 19:21:52 +0300

    zada4a3

---
+++ task23/Program.cs
@@ -0,0 +1,16 @@
+﻿int N = Prompt("Введите число: ");
+SquareNum(N);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+void SquareNum(int num)
+{                                                                   
+    for (int i = 1; i <= num; i++)                                  
+    {                                                               
+        Console.Write(Math.Pow(i, 3) + " ");                        
+    }                                                               
+}
