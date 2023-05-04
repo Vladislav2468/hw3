2ed994016eca63b3e05c71c7e1437b6f00c3e1d1
Vladislav Ivanov <lolowkapogba2468@gmail.com>
2023-05-04 19:19:54 +0300

    zada4a1

---
+++ task19/Program.cs
@@ -0,0 +1,33 @@
+﻿int number = Prompt("Введите число: ");
+int numberMirr = FindMirrorNumber(number);
+FindPalindron(number, numberMirr);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int mess = int.Parse(Console.ReadLine()!);
+    return mess;
+}
+
+int FindMirrorNumber(int x)
+{
+    int result = 0;
+    while (x > 0)
+    {
+        result = result * 10 + x % 10;
+        x /= 10;
+    }
+    return result;
+}
+
+void FindPalindron(int a, int b)
+{
+    if (number == numberMirr)
+    {
+        Console.Write("YES");
+    }
+    else
+    {
+        Console.Write("NO");
+    }
+}
